3ff5b55e094c3d581dc0bd36b79a18429cba4d0a
anushkakulk <115674789+anushkakulk@users.noreply.github.com>
2024-01-19 10:04:18 -0500

    fixed deployment error

--- pages/reversi.jsx
+++ pages/reversi.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import reversi from "../public/assets/projects/rev.gif";
+// import reversi from "../public/assets/projects/rev.gif";
 import { BsCodeSlash, BsArrowRight } from "react-icons/bs";
 import Link from "next/link";
 
